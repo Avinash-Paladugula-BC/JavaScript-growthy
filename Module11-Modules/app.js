7a2ee66961383bc4cf415e54c1f4258535959ca0
avinash <avinash.paladugula@beautifulcode.in>
2025-11-12 17:29:36 +0530

    added module11-activity importing and exporting modules in js

---
+++ Module11-Modules/app.js
@@ -0,0 +1,9 @@
+// named import
+import {add} from "./mathUtils.js";
+
+// default import
+import multiply from "./mathUtils.js";
+
+// calling the imported methdos with the values
+console.log(add(1,2));
+console.log(multiply(2,3));
